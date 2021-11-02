a64281fafe0bd7556b2a1cac60f8cb7f7d5a1081
Sreerag M S <45370094+sreerag-ms@users.noreply.github.com>
2021-11-02 21:10:40 +0530

    Build user model (#22)
    * Added users table creating migration, added index for email
    
    * Added User model and required validations
    
    * Added test methods for user model
    
    * Disabled fixture auto generation

---
+++ config/initializers/constants.rb
@@ -0,0 +1,5 @@
+module Constants
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  MAX_FIRST_NAME_LENGTH = 50
+  MAX_LAST_NAME_LENGTH = 50
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,67 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(email: 'falcon@spacex.com',
+                     first_name: 'falcon',
+                     last_name: 'nine')
+  end
+
+  def test_user_valid
+    assert @user.valid?
+  end
+
+  def test_user_invalid_email
+    @user.email = nil
+    assert_not @user.valid?
+  end
+
+  def test_user_invalid_first_name
+    @user.first_name = nil
+    assert_not @user.valid?
+  end
+
+  def test_user_invalid_last_name
+    @user.last_name = nil
+    assert_not @user.valid?
+  end
+
+  def test_user_invalid_email_format
+    invalid_emails = %w[@.com,user@example,com user_at_example.org user.name@example.
+                        @sam-sam.com sam@sam+exam.com fishy+#.com]
+    invalid_emails.each do |email|
+      @user.email = email
+      assert @user.invalid?
+    end
+  end
+
+  def test_user_invalid_email_and_first_name_and_last_name
+    @user.email = nil
+    @user.first_name = nil
+    @user.last_name = nil
+    assert_not @user.valid?
+  end
+
+  def test_user_invalid_max_length_first_name
+    @user.first_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  def test_user_invalid_max_length_last_name
+    @user.last_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  def test_user_uniqueness_email
+    @user.save
+    @user2 = User.new(email: 'falcon@spacex.com', first_name: 'raptor', last_name: 'sn1')
+    assert @user2.invalid?
+  end
+
+  def test_user_uniqueness_email_case_insensitive
+    @user.email = 'Falcon@spacex.com'
+    @user.save
+    @user2 = User.new(email: 'falcon@spacex.com', first_name: 'raptor', last_name: 'sn1')
+    assert_not @user2.valid?
+  end
+end
