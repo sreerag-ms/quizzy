2b11c92125ba097166209f6caec7518b588cef85
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-02 20:38:09 +0530

    Disabled fixture auto generation

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,10 @@ module QuizzyBySreeragMs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
