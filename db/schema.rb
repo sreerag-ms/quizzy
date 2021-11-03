164e89f3cd104bc3ff2967dd07a19237f2995225
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-03 19:16:20 +0530

    Added authentication_token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_095016) do
+ActiveRecord::Schema.define(version: 2021_11_03_124133) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_095016) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role", default: 0, null: false
     t.string "password_digest", null: false
+    t.string "authentication_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
