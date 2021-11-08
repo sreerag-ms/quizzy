cc8dbba6682c6923d9b781e3e874da501a2ff33c
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-08 20:06:26 +0530

    Added testing for Quiz model

--- test/models/quiz_test.rb
+++ test/models/quiz_test.rb
@@ -3,7 +3,31 @@
 require "test_helper"
 
 class QuizTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      email: "falcon@spacex.com",
+      first_name: "falcon",
+      last_name: "nine",
+      password: "password",
+      password_confirmation: "password"
+    )
+    @user.save!
+    @quiz = @user.quizzes.new(name: "sample quiz")
+  end
+
+  def test_valid
+    assert @quiz.valid?
+  end
+
+  def test_invalid_without_name
+    @quiz.name = nil
+    assert_not @quiz.valid?
+    assert_includes @quiz.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_quiz_invalid_without_valid_user
+    @quiz.user_id = -1
+    assert_not @quiz.save
+    assert_includes @quiz.errors.full_messages, "User must exist"
+  end
 end
