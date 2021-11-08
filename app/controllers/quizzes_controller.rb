5d9b483fb90bb50900ef04150ec1f354f2e94b1e
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-08 16:49:04 +0530

    Added update and destroy methods to Quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,6 +2,7 @@
 
 class QuizzesController < ApplicationController
   before_action :authenticate_user_using_x_auth_token
+  before_action :load_quiz, only: [:update, :destroy]
 
   def create
     quiz = @current_user.quizzes.new(quiz_params)
@@ -12,9 +13,34 @@ class QuizzesController < ApplicationController
     end
   end
 
+  def index
+    @quizzes = Quiz.all
+    puts "quizzes: #{@quizzes}"
+  end
+
+  def update
+    if @quiz.update(quiz_params)
+      render status: :ok, json: { notice: "Quiz updated successfully" }
+    else
+      render status: 422, json: { notice: "Could not update quiz" }
+    end
+  end
+
+  def destroy
+    if @quiz.destroy
+      render status: :ok, json: { notice: "Quiz deleted successfully" }
+    else
+      render status: 422, json: { notice: "Could not delete quiz" }
+    end
+  end
+
   private
 
     def quiz_params
       params.require(:quiz).permit(:name)
+    end
+
+    def load_quiz
+      @quiz = Quiz.find(params[:id])
     end
 end
